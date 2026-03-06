33b781afb0c7e54f853da230e0e392ed15ad2163
VegaBoxADMIN <darthroket12@gmail.com>
2026-03-06 10:03:22 +1000

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const params = new URLSearchParams(window.location.search);
+
 const video = params.get("video");
+const title = params.get("title");
+const episode = params.get("episode");
 
 const player = document.getElementById("videoPlayer");
+const showTitle = document.getElementById("showTitle");
+const episodeLabel = document.getElementById("episodeLabel");
 
-if (video) {
-    player.src = video;
-} else {
-    player.innerHTML = "Video not found.";
-}
+if (video) player.src = video;
+if (title) showTitle.textContent = title;
+if (episode) episodeLabel.textContent = "Episode " + episode;
